f0d73d4ea620fc8038147fe2fb1b2cfabccb3e22
Christoph Heindl <christoph.heindl@gmail.com>
2022-05-31 17:57:08 +0200

    added timing

--- surfacenets/surface_nets.py
+++ surfacenets/surface_nets.py
@@ -42,43 +42,62 @@ def surface_net(
     ).astype(np.int32)
 
     # Compute voxel edge values as (VX,VY,VZ,12,2) array
-    edges = np.empty((VX, VY, VZ, 12, 2), dtype=np.float32)
+    edge_sdfs = np.empty((VX, VY, VZ, 12, 2), dtype=np.float32)
     for eidx, (source_offset, target_offset) in enumerate(VOXEL_EDGES):
         sidx = voxids + source_offset
         tidx = voxids + target_offset
-        edges[..., eidx, 0] = sdf_values[sidx[..., 0], sidx[..., 1], sidx[..., 2]]
-        edges[..., eidx, 1] = sdf_values[tidx[..., 0], tidx[..., 1], tidx[..., 2]]
+        edge_sdfs[..., eidx, 0] = sdf_values[sidx[..., 0], sidx[..., 1], sidx[..., 2]]
+        edge_sdfs[..., eidx, 1] = sdf_values[tidx[..., 0], tidx[..., 1], tidx[..., 2]]
 
     # Find the voxels through which the surface passes.
     # We do this by finding voxels that have at least one SDF corner value
     # that is different from the rest.
-    signs = np.sign(edges)
+    signs = np.sign(edge_sdfs)
     selected_edges = abs(signs.sum(-1)) != 2  # (VX,VY,VZ,12)
     selected_voxels = selected_edges.any(-1)  # (VX,VY,VZ)
 
     # Compute the intersection points within the voxel for all selected voxels
     if vertex_placement_mode == "midpoint":
-        # Assume constant resultion
+        # Assume constant spacing, so to simplify midpoint computation
         spacing = xyz[1, 1, 1] - xyz[0, 0, 0]
         x, y, z = np.where(selected_voxels)
         verts = xyz[x, y, z] + spacing[None, :] * 0.5
-    else:
-        edge_isects = np.zeros((VX, VY, VZ, 12, 3), dtype=np.float32)
+    elif vertex_placement_mode == "naive":
+        # We compute the vertex locations per voxel as the mean of the voxel
+        # edge-surface intersections.
+        edge_isects = np.empty(
+            (VX, VY, VZ, 12, 3), dtype=np.float32
+        )  # TODO: should be sparse, but how to accumulate over voxels then?
         x, y, z, e = np.where(selected_edges)
-        t = -edges[x, y, z, e, 0] / (edges[x, y, z, e, 1] - edges[x, y, z, e, 0])
-        source_offsets = VOXEL_EDGES[e][:, 0]
-        target_offsets = VOXEL_EDGES[e][:, 1]
 
+        # Compute surface intersection points along selected edges. We assume
+        # that the surface is smooth and linear close to an edge. Hence, we can
+        # find the linear interpolation value t [0,1] along the selected edge
+        # for which SDF should be 0. We then use t to mix start and end point
+        # of edges to compute the intersection point. Finally, we determine a
+        # single vertex per selected voxel as the mean of all intersection points
+        # corresponding to an voxel edge.
+        t = -edge_sdfs[x, y, z, e, 0] / (
+            edge_sdfs[x, y, z, e, 1] - edge_sdfs[x, y, z, e, 0]
+        )
+        source_offsets = VOXEL_EDGES[e, 0]
+        target_offsets = VOXEL_EDGES[e, 1]
+
+        # Compute all the start/end vertices for each selected edge
         vsource = xyz[
             x + source_offsets[:, 0], y + source_offsets[:, 1], z + source_offsets[:, 2]
         ]
         vtarget = xyz[
             x + target_offsets[:, 0], y + target_offsets[:, 1], z + target_offsets[:, 2]
         ]
+        # Find the intersection points using linear interpolation
         isects = (1 - t[:, None]) * vsource + t[:, None] * vtarget
+
+        # Write back to dense array for accumulation
         edge_isects[x, y, z, e] = isects
-        sum_edges = selected_edges.sum(-1, keepdims=True)
-        verts = edge_isects[selected_voxels].sum(-2) / sum_edges[selected_voxels]
+        num_edges = selected_edges.sum(-1, keepdims=True)
+        # Compute vertex locations as mean over voxel edge intersection points
+        verts = edge_isects[selected_voxels].sum(-2) / num_edges[selected_voxels]
         return verts
 
     return verts
@@ -88,6 +107,7 @@ if __name__ == "__main__":
     import matplotlib.pyplot as plt
     from skimage.measure import marching_cubes
     from . import sdfs, plots
+    import time
 
     res = (40, 40, 40)
     min_corner = np.array([-2.0] * 3, dtype=np.float32)
@@ -116,11 +136,13 @@ if __name__ == "__main__":
     fig, ax = plots.create_figure()
     # plots.plot_mesh(verts, faces, ax)
 
+    t0 = time.perf_counter()
     verts = surface_net(s1(xyz), xyz, vertex_placement_mode="naive")
-    ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2])
+    ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2], s=5)
+    print("Surface-nets took", time.perf_counter() - t0, "secs")
 
-    # verts = surface_net(s1(xyz), xyz, vertex_placement_mode="midpoint")
-    # ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2])
+    verts = surface_net(s1(xyz), xyz, vertex_placement_mode="midpoint")
+    ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2], s=5)
 
     plots.setup_axes(ax, min_corner, max_corner)
     plt.show()

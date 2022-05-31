7894e4b0ce912177b0afd94029cd7aa89582f479
Christoph Heindl <christoph.heindl@gmail.com>
2022-05-31 17:43:23 +0200

    added support for naive vertex placement

--- surfacenets/surface_nets.py
+++ surfacenets/surface_nets.py
@@ -1,22 +1,27 @@
 import numpy as np
 
-VOXEL_EDGES = (
-    ((0, 0, 0), (1, 0, 0)),  # Front face loop
-    ((1, 0, 0), (1, 0, 1)),
-    ((1, 0, 1), (0, 0, 1)),
-    ((0, 0, 1), (0, 0, 0)),
-    ((0, 1, 0), (1, 1, 0)),  # Back face loop
-    ((1, 1, 0), (1, 1, 1)),
-    ((1, 1, 1), (0, 1, 1)),
-    ((0, 1, 1), (0, 1, 0)),
-    ((0, 0, 0), (0, 1, 0)),  # Connecting edges
-    ((1, 0, 0), (1, 1, 0)),
-    ((1, 0, 1), (1, 1, 1)),
-    ((0, 0, 1), (0, 1, 1)),
+VOXEL_EDGES = np.array(
+    (
+        ((0, 0, 0), (1, 0, 0)),  # Front face loop
+        ((1, 0, 0), (1, 0, 1)),
+        ((1, 0, 1), (0, 0, 1)),
+        ((0, 0, 1), (0, 0, 0)),
+        ((0, 1, 0), (1, 1, 0)),  # Back face loop
+        ((1, 1, 0), (1, 1, 1)),
+        ((1, 1, 1), (0, 1, 1)),
+        ((0, 1, 1), (0, 1, 0)),
+        ((0, 0, 0), (0, 1, 0)),  # Connecting edges
+        ((1, 0, 0), (1, 1, 0)),
+        ((1, 0, 1), (1, 1, 1)),
+        ((0, 0, 1), (0, 1, 1)),
+    ),
+    dtype=np.int32,
 )
 
 
-def surface_net(sdf_values: np.ndarray, xyz: np.ndarray):
+def surface_net(
+    sdf_values: np.ndarray, xyz: np.ndarray, vertex_placement_mode="midpoint"
+):
     """Generate surface net triangulation
 
     Params
@@ -38,9 +43,9 @@ def surface_net(sdf_values: np.ndarray, xyz: np.ndarray):
 
     # Compute voxel edge values as (VX,VY,VZ,12,2) array
     edges = np.empty((VX, VY, VZ, 12, 2), dtype=np.float32)
-    for eidx, (s, t) in enumerate(VOXEL_EDGES):
-        sidx = voxids + s
-        tidx = voxids + t
+    for eidx, (source_offset, target_offset) in enumerate(VOXEL_EDGES):
+        sidx = voxids + source_offset
+        tidx = voxids + target_offset
         edges[..., eidx, 0] = sdf_values[sidx[..., 0], sidx[..., 1], sidx[..., 2]]
         edges[..., eidx, 1] = sdf_values[tidx[..., 0], tidx[..., 1], tidx[..., 2]]
 
@@ -48,12 +53,33 @@ def surface_net(sdf_values: np.ndarray, xyz: np.ndarray):
     # We do this by finding voxels that have at least one SDF corner value
     # that is different from the rest.
     signs = np.sign(edges)
-    counts = abs(signs.sum(axis=(-1, -2)))
-    selected = voxids[counts != 24]
+    selected_edges = abs(signs.sum(-1)) != 2  # (VX,VY,VZ,12)
+    selected_voxels = selected_edges.any(-1)  # (VX,VY,VZ)
 
-    # Compute the intersection points within the voxel
-    # for all selected voxels
-    verts = xyz[selected[:, 0], selected[:, 1], selected[:, 2]]
+    # Compute the intersection points within the voxel for all selected voxels
+    if vertex_placement_mode == "midpoint":
+        # Assume constant resultion
+        spacing = xyz[1, 1, 1] - xyz[0, 0, 0]
+        x, y, z = np.where(selected_voxels)
+        verts = xyz[x, y, z] + spacing[None, :] * 0.5
+    else:
+        edge_isects = np.zeros((VX, VY, VZ, 12, 3), dtype=np.float32)
+        x, y, z, e = np.where(selected_edges)
+        t = -edges[x, y, z, e, 0] / (edges[x, y, z, e, 1] - edges[x, y, z, e, 0])
+        source_offsets = VOXEL_EDGES[e][:, 0]
+        target_offsets = VOXEL_EDGES[e][:, 1]
+
+        vsource = xyz[
+            x + source_offsets[:, 0], y + source_offsets[:, 1], z + source_offsets[:, 2]
+        ]
+        vtarget = xyz[
+            x + target_offsets[:, 0], y + target_offsets[:, 1], z + target_offsets[:, 2]
+        ]
+        isects = (1 - t[:, None]) * vsource + t[:, None] * vtarget
+        edge_isects[x, y, z, e] = isects
+        sum_edges = selected_edges.sum(-1, keepdims=True)
+        verts = edge_isects[selected_voxels].sum(-2) / sum_edges[selected_voxels]
+        return verts
 
     return verts
 
@@ -88,11 +114,13 @@ if __name__ == "__main__":
     verts += min_corner[None, :]
 
     fig, ax = plots.create_figure()
-    plots.plot_mesh(verts, faces, ax)
+    # plots.plot_mesh(verts, faces, ax)
 
-    verts = surface_net(s1(xyz), xyz)
-    verts += np.array(spacing) * 0.5
+    verts = surface_net(s1(xyz), xyz, vertex_placement_mode="naive")
     ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2])
+
+    # verts = surface_net(s1(xyz), xyz, vertex_placement_mode="midpoint")
+    # ax.scatter(verts[:, 0], verts[:, 1], verts[:, 2])
 
     plots.setup_axes(ax, min_corner, max_corner)
     plt.show()

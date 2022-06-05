138a65b32da1cf191bff5ed9ec8d0b8861b19dfc
Christoph Heindl <christoph.heindl@gmail.com>
2022-06-05 08:12:09 +0200

    docs updated

--- surfacenets/utils.py
+++ surfacenets/utils.py
@@ -2,4 +2,10 @@ import numpy as np
 
 
 def reorient_volume(x: np.ndarray):
+    """Reorients the given volume for easier inspection.
+
+    In particular the volume is reoriented, so that when printing the volume
+    you see ij-slices (k from from front to back) and each ij-slice has the origin
+    at lower left.
+    """
     return np.flip(x.transpose((2, 1, 0)), 1)

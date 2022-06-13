8c355219909d76f11d798d7d0ca80e2c0767b85d
Christoph Heindl <christoph.heindl@gmail.com>
2022-06-13 14:23:50 +0200

    updated example

--- examples/debug_cube.py
+++ examples/debug_cube.py
@@ -28,15 +28,16 @@ def get_rotated_box(rot):
 def main():
 
     # Setup the scene
-    scene = sn.sdfs.Box((1.1, 1.1, 1.1)).transform(rot=(1.0, 1.0, 0, np.pi / 4))
-    grid = sn.Grid((32, 32, 32))
+    rot = (1.0, 1.0, 1.0, np.pi / 4)
+    scene = sn.sdfs.Box((1.1, 1.1, 1.1)).transform(rot=rot)
+    grid = sn.Grid((3, 3, 3))
 
     # Generate mesh
     verts, faces, debug = sn.dual_isosurface(
         scene,
         grid,
-        vertex_strategy=sn.DualContouringVertexStrategy(),
-        # edge_strategy=sn.NewtonEdgeStrategy(max_steps=50),
+        # vertex_strategy=sn.DualContouringVertexStrategy(),
+        # edge_strategy=sn.BisectionEdgeStrategy(max_steps=50),
         triangulate=False,
         return_debug_info=True,
     )
@@ -52,14 +53,22 @@ def main():
         verts, faces
     )  # face_normals, vert_normals)
 
-    # v, f = get_rotated_box((1.0, 1.0, 0, np.pi / 4))
-    # sn.plotting.plot_mesh(ax, v, f, alpha=0.5)
+    v, f = get_rotated_box(rot)
+    sn.plotting.plot_mesh(ax, v, f, alpha=0.1, color="gray")
 
     isect = grid.grid_to_data(debug.edges_isect_coords[debug.edges_active_mask])
     isect_n = scene.gradient(isect, normalize=True)
-    # sn.plotting.plot_normals(ax, isect, isect_n, "yellow")
 
-    # sn.plotting.plot_samples(ax, grid.xyz, scene.sample(grid.xyz))
+    active_src, active_dst = grid.find_edge_vertices(
+        np.where(debug.edges_active_mask)[0], ravel=False
+    )
+    print(active_src, active_dst)
+    active_src = grid.grid_to_data(active_src)
+    active_dst = grid.grid_to_data(active_dst)
+    sn.plotting.plot_edges(ax, active_src, active_dst, color="yellow", linewidth=0.5)
+    sn.plotting.plot_normals(ax, isect, isect_n, "yellow")
+
+    sn.plotting.plot_samples(ax, grid.xyz, scene.sample(grid.xyz))
     sn.plotting.setup_axes(ax, grid.min_corner, grid.max_corner)
     # sn.plotting.generate_rotation_gif("normals.gif", fig, ax)
     plt.show()

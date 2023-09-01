35e51089140d420436e6584f40125970c528c7ef
Christoph Heindl <christoph.heindl@gmail.com>
2023-09-01 08:19:11 +0200

    added example that converts a density image (ngp/neus2) to a mesh

--- examples/neus2mesh.py
+++ examples/neus2mesh.py
@@ -8,10 +8,9 @@ def main():
     import sdftoolbox
 
     # Generate the sampling locations. Here we use the default params
-    res = [64] * 3
-    th = 0.0
+    res = [256] * 3
     drange = 4.0
-    fpath = "density_A.png"
+    fpath = "density_e.png"
     grid = sdftoolbox.Grid(
         res=res,
         min_corner=(-1.5, -1.5, -1.5),
@@ -19,10 +18,7 @@ def main():
     )
 
     sdfvalues = sdftoolbox.io.import_volume_from_density_image(
-        fpath,
-        res,
-        drange,
-        th,
+        fpath, res, drange, flip=True
     )
 
     scene = sdftoolbox.sdfs.Discretized(grid, sdfvalues)
@@ -31,19 +27,28 @@ def main():
     verts, faces = sdftoolbox.dual_isosurface(
         scene,
         grid,
-        vertex_strategy=sdftoolbox.DualContouringVertexStrategy(),
         triangulate=False,
     )
 
     # Export
-    sdftoolbox.io.export_stl("surfacenets.stl", verts, faces)
+    sdftoolbox.io.export_stl("dual.stl", verts, faces)
+
+    # from skimage.measure import marching_cubes
+
+    # verts_mc, faces_mc, _, _ = marching_cubes(
+    #     sdfvalues,
+    #     0.0,
+    #     spacing=grid.spacing,
+    # )
+    # verts_mc += grid.min_corner
+    # sdftoolbox.io.export_stl("mc.stl", verts_mc, faces_mc)
 
     # Visualize
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
-    # plt.style.use("dark_background")
-    fig, ax = sdftoolbox.plotting.create_mesh_figure(verts, faces)
-    plt.show()
+    # # plt.style.use("dark_background")
+    # fig, ax = sdftoolbox.plotting.create_mesh_figure(verts, faces)
+    # plt.show()
 
 
 if __name__ == "__main__":
